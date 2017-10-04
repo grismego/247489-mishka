5ce2da92434efeabbcbf987119560edee0d5a3cc
grismego <gris.mego@gmail.com>
2017-10-05 02:05:02 +0700

    исправлеяет стили

--- js/javascript.js
+++ js/javascript.js
@@ -3,8 +3,29 @@ var menu_close = document.querySelector(".header-nav__toggle--close");
 var menu = document.querySelector(".header-nav__toggle");
 var site_list = document.querySelector(".site-list");
 
+function remove_css_btn() {
+  var menu = document.querySelector(".header-nav__toggle");
+  if (menu.classList.contains("header-nav__toggle--no-js")) {
+    console.log("+++");
+    menu.classList.remove("header-nav__toggle--no-js");
+  } else {
+    console.log("---");
+  }
+}
+remove_css_btn();
 
-menu_open.addEventListener("click", function(event){
+function remove_css_menu() {
+  var site_list = document.querySelector(".site-list");
+  if (site_list.classList.contains("site-list--no-js")) {
+    console.log("+++");
+    site_list.classList.remove("site-list--no-js");
+  } else {
+    console.log("---");
+  }
+}
+remove_css_menu();
+
+menu_open.addEventListener("click", function(event) {
   event.preventDefault();
   menu.classList.toggle("header-nav__toggle--close");
   site_list.classList.toggle("site-list--show");
